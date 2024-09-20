45d8717579cfac28e432a6dba199ffe3c1a6b923
Giorgi Mikautadze <giorgi.mikautadze2223@gmail.com>
2024-09-20 15:01:34 +0400

    feat: started clear filter functionality

--- src/app/components/Filter/Filter.tsx
+++ src/app/components/Filter/Filter.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+"use client"
+import React, { useContext } from 'react'
 import FilterProperties from './FilterProperties'
 import AddingApplicationsButtons from '../AddingApplicationsButtons'
+import { globalContext } from '@/app/context/globalContext'
+import CloseIcon from "../../assets/x.svg"
+import Image from 'next/image'
 
 const Filter = () => {
+    const context = useContext(globalContext);
+
     return (
         <div className='font-bold text-[16px]'>
             <FilterProperties />
+            <div className='mt-5 flex gap-5'>
+                {context?.selectedRegion.map((region) => (
+                    <div className={`border-[1px] w-max ${context?.selectedRegion.length === 0 ? "hidden" : "block"} border-[#DBDBDB] py-[5px] px-[20px] flex gap-1 items-center justify-between rounded-[43px]`}>
+                        <p>{region}</p>
+                        <Image src={CloseIcon} alt='close' className='cursor-pointer' onClick={() => context?.setSelectedRegion(context?.selectedRegion.filter((item) => item !== region))} />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
